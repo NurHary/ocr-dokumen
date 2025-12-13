d20bda4ee6b9e16c11a566292dcdd1b450d344c5
NurHary <theliq310@gmail.com>
2025-12-13 15:02:02 +0700

    ganti http

--- app/src/main/java/com/example/ocrdokumen/MainActivity.kt
+++ app/src/main/java/com/example/ocrdokumen/MainActivity.kt
@@ -1,28 +1,18 @@
 package com.example.ocrdokumen
 
-import android.app.Dialog
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothManager
-import android.graphics.drawable.shapes.Shape
-import android.icu.text.CaseMap
 import android.os.Bundle
 import android.content.Intent
 import android.content.IntentFilter
 import android.content.BroadcastReceiver
 import android.content.Context
-import android.widget.Button
-import android.widget.ScrollView
-import android.widget.Scroller
+import android.location.LocationManager
+import android.provider.Settings
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.result.ActivityResult
-import androidx.activity.result.ActivityResultCallback
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContract
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.activity.result.registerForActivityResult
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -30,60 +20,40 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Shapes
 import androidx.compose.material3.Text
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.SnackbarHost
-import androidx.compose.material3.Snackbar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.unit.dp
 import com.example.ocrdokumen.ui.theme.OcrDokumenTheme
-import org.w3c.dom.Text
 
-import androidx.camera.core.*
-import androidx.camera.lifecycle.ProcessCameraProvider
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Card
-import androidx.compose.material3.ScrollableTabRow
 import androidx.compose.material3.TextButton
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
-import androidx.core.view.ScrollingView
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import kotlinx.coroutines.launch
 
 const val REQUEST_ENABLE_BLUETOOTH: Int = 255091
+const val REQUEST_ENABLE_GPS: Int = 255555
 
 class MainActivity : ComponentActivity() {
     var GBluetoothAvailableDevices = mutableStateListOf<BluetoothDevice?>()
@@ -97,7 +67,11 @@ class MainActivity : ComponentActivity() {
             OcrDokumenTheme {
 
                 val bluetoothManager: BluetoothManager = getSystemService(BluetoothManager::class.java)
+                val locationManager: LocationManager = getSystemService(LocationManager::class.java)
+
                 val bluetoothAdapter : BluetoothAdapter? = bluetoothManager.adapter;
+                var isGpsEnabled : Boolean = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+
 
 //                var bt_activity_launcher: ActivityResultLauncher<Intent> = registerForActivityResult(
 //                    ActivityResultContracts.StartActivityForResult(),
@@ -118,22 +92,16 @@ class MainActivity : ComponentActivity() {
                 // Untuk Card Foto
                 var confirmPhoto = remember { mutableStateOf(false) }
                 // Untuk Card Bluetooth
-                var bluetoothConnectedDevice = remember { mutableStateOf(false) }
-
-
-                var availableBluetoothDevice : MutableState<Set<BluetoothDevice>> = remember { mutableStateOf(setOf())}
-
-                if (bluetoothAdapter == null) {
-                    // Hp Kentang
-                    println("Device Tidak Support BLuetooth / HP KENTANG");
-                }
+                var wifiConnect = remember { mutableStateOf(false) }
 
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
 
 
                     // Main Layout
                     Box (
-                        modifier = Modifier.padding(innerPadding).fillMaxSize()
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .fillMaxSize()
                     ) {
 
                         // SnackBar
@@ -165,13 +133,17 @@ class MainActivity : ComponentActivity() {
                                                 // TODO: Pindahkan layar / timpa dan kasih opsi untuk membenarkan Bluetooth
 
                                                 // Perangkat Yang Telah Disambungkaval
-                                                val requestCode = 1;
-                                                val discoverableIntent: Intent = Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE).apply {
-                                                   putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300)
+//                                                val requestCode = 1;
+//                                                val discoverableIntent: Intent = Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE).apply {
+//                                                   putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300)
+//                                                }
+//                                                startActivityForResult(discoverableIntent, requestCode)
+                                                // ini untuk menjadikan hp ini yang discoverable
+                                                if (!isGpsEnabled){
+                                                    val mapIntent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                                                    startActivityForResult(mapIntent, REQUEST_ENABLE_GPS)
                                                 }
-                                                startActivityForResult(discoverableIntent, requestCode)
-                                                bluetoothAdapter.startDiscovery()
-                                                bluetoothConnectedDevice.value = true
+                                                wifiConnect.value = true
                                             }
                                         }
 
@@ -180,14 +152,14 @@ class MainActivity : ComponentActivity() {
                                     shape= RectangleShape,
                                     modifier = Modifier.padding( all = 10.dp)
                                 ) {
-                                    Text(text = "Izinkan Bluetooth")
+                                    Text(text = "Connect Perangkat")
                                 }
                                 when {
-                                    bluetoothConnectedDevice.value -> { // TODO: &&
+                                    wifiConnect.value -> { // TODO: &&
                                         BluetoothConnectToDevice(
-                                            onDismissRequest = {bluetoothConnectedDevice.value = false},
+                                            onDismissRequest = {wifiConnect.value = false},
                                             onConfirmation = {
-                                                bluetoothConnectedDevice.value = false
+                                                wifiConnect.value = false
                                                 // NOTES
                                                 // Kirim Ke Bluetooth
                                                 println("Mengirim Ke Bluetooth")
@@ -245,7 +217,9 @@ class MainActivity : ComponentActivity() {
                         intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
                     GBluetoothAvailableDevices.add(device)
                     val deviceName = device?.name
+                    println(deviceName)
                     val deviceHardwareAddress = device?.address // MAC address
+                    println(deviceHardwareAddress)
                 }
             }
         }
@@ -317,11 +291,11 @@ fun BluetoothConnectToDevice(
     btadapter: BluetoothAdapter?,
     btdevice: SnapshotStateList<BluetoothDevice?>,
 ){
-    val pairedDevices: Set<BluetoothDevice>? = btadapter?.bondedDevices
-    pairedDevices?.forEach { device ->
-        val deviceName = device.name
-        val deviceHardwareAddress = device.address // MAC address
-    }
+//    val pairedDevices: Set<BluetoothDevice>? = btadapter?.bondedDevices
+//    pairedDevices?.forEach { device ->
+//        val deviceName = device.name
+//        val deviceHardwareAddress = device.address // MAC address
+//    }
     Dialog(onDismissRequest = { onDismissRequest() }) {
         Card (
             modifier = Modifier
@@ -339,8 +313,8 @@ fun BluetoothConnectToDevice(
                 .height(600.dp)
                 .width(750.dp)
                 .verticalScroll(
-                rememberScrollState()
-            ))
+                    rememberScrollState()
+                ))
             {
                 // Jaringan Terkoneksi
                 Text(text= "Koneksi Bluetooth Saat Ini   ···················", modifier = Modifier.fillMaxWidth(), color = Color.DarkGray)
@@ -351,7 +325,9 @@ fun BluetoothConnectToDevice(
                     bluetooth_box("Bahlil", "Etanol", {jajal2()})
                 }
             }
-            Row(modifier = Modifier.padding(15.dp).fillMaxWidth(), verticalAlignment = Alignment.Bottom, horizontalArrangement = Arrangement.Absolute.SpaceEvenly) {
+            Row(modifier = Modifier
+                .padding(15.dp)
+                .fillMaxWidth(), verticalAlignment = Alignment.Bottom, horizontalArrangement = Arrangement.Absolute.SpaceEvenly) {
                 Button(shape = RoundedCornerShape(4.dp), onClick = {}) { Text("Refresh", fontSize = 12.sp)}
                 TextButton(
                     onClick = {
